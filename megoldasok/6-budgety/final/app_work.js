745a3e01af0e3579c3790a75cf889d6c79d9e4e5
milesdavis71 <OttosMops71>
2020-06-30 19:01:08 +0200

    6/7 levke gyakorlás kész.

--- megoldasok/6-budgety/final/app_work.js
+++ megoldasok/6-budgety/final/app_work.js
@@ -5,12 +5,15 @@ var budgetController = (function () {
 // Ez egy IFEE
 var UIController = (function () {
     
-    // ez az objektum arra jó, hogy ha a html-ben más lesz az osztály neve,
-    // akkor könnyen megváltoztatható legyen itt, a js kódban is.
+    // ez az objektum arra jó, hogy ha a html-ben lévő DOM stringek (osztály nevek) megváltoznak, akkor a DOM stringek könnyen megváltoztathatók legyenek itt, a js kódban is.
+    // 3 DOM string van az UIController függvényben, és 1 a controller függvényben. Ez az objektum viszont az UIControllerben van. Valahogy meg kell mutatni a másik függvénynek is.
+    // Ezt az objekutmot át kell adni az egyik osztályból a másikba.
+
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add__value'
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     };
 
     // a returnben három változót akarunk egyszerre visszakapni, erre az a legjobb
@@ -24,12 +27,17 @@ var UIController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value
 
             };
+        }, // <-- Ide kell egy vessző, nézz utána, miért.
+        // ez a return megmutatja a privát DOMStrings objektumot a többi (publikus) függvénynek
+        getDOMStrings: function() {
+            return DOMstrings;
         }
 
     };
 })();
 // Ez maga a kontroller, itt lesz meghívva a többi modul.
 var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UIController.getDOMStrings();
     var ctrlAdditem = function () {
         // Ez publikus, ez érhető el kívülről.
         // 1. beolvasni az UI-ról mező adatokat
@@ -39,7 +47,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var input = UICtrl.getInput();
 
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAdditem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAdditem);
     document.addEventListener('keypass', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
